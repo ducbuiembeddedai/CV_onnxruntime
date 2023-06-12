f1958f350776dddda2a03a06553dd124534b48d6
Dmitri Smirnov <dmitrism@microsoft.com>
2023-06-12 15:30:34 -0700

    Introduce Float1/BFloat16 support and tests for C#

--- csharp/test/Microsoft.ML.OnnxRuntime.Tests.Droid/Resources/Resource.designer.cs
+++ csharp/test/Microsoft.ML.OnnxRuntime.Tests.Droid/Resources/Resource.designer.cs
@@ -13,8 +13,8 @@
 namespace Microsoft.ML.OnnxRuntime.Tests.Droid
 {
 	
-
-	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "1.0.0.0")]
+	
+	[global::System.CodeDom.Compiler.GeneratedCodeAttribute("Xamarin.Android.Build.Tasks", "13.2.0.99")]
 	public partial class Resource
 	{
 		
